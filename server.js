85c45bdfb468d849d5bba175f5c10539b991cced
maancham <houmaanch@gmail.com>
2023-06-24 07:45:29 -0300

    done server

--- server.js
+++ server.js
@@ -1,56 +1,31 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const { exec } = require('child_process');
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// app.post('/runScript', (req, res) => {
-//     exec('./run.sh', (error, stdout, stderr) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return res.status(500).send({ error: error.message });
-//         }
-//         if (stderr) {
-//             console.log(`stderr: ${stderr}`);
-//             return res.status(500).send({ error: stderr });
-//         }
-//         console.log(`stdout: ${stdout}`);
-//         return res.send({ message: stdout });
-//     });
-// });
-
-// app.listen(3001, () => console.log('Server is running on port 3001'));
-
 const express = require("express");
+const bodyParser = require("body-parser");
 const { spawn } = require("child_process");
+const cors = require("cors");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get("/runScript", (req, res) => {
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
-  res.setHeader("Transfer-Encoding", "chunked");
+app.post("/runScript", (req, res) => {
+  let { origin, operator, first, second } = req.body;
 
-  const scriptExecution = spawn("./run.sh");
+  const command = `./run.sh ${origin} ${operator} ${first} ${second}`;
 
-  scriptExecution.stdout.on("data", (data) => {
-    const output = data.toString().trim();
-    res.write(`data: ${output}\n\n`);
+  const childProcess = spawn(command, { shell: true });
+
+  childProcess.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+    res.write(data);
   });
 
-  scriptExecution.stderr.on("data", (data) => {
-    const error = data.toString().trim();
-    res.write(`data: Error: ${error}\n\n`);
+  childProcess.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+    res.write(data);
   });
 
-  scriptExecution.on("close", (code) => {
-    if (code === 0) {
-      res.write("data: Script execution completed successfully\n\n");
-    } else {
-      res.write(`data: Script execution failed with exit code ${code}\n\n`);
-    }
+  childProcess.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
     res.end();
   });
 });
